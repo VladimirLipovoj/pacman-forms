3d8eb6b0e859761016acbd3c5d11e9e3ddc726d2
VladimirLipovoj <v_lipovoj@mail.ru>
2017-11-05 16:09:56 +0300

    Fixed some comments

--- PacmanForms/Common/Main.cs
+++ PacmanForms/Common/Main.cs
@@ -8,9 +8,7 @@ namespace PacmanForms
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
